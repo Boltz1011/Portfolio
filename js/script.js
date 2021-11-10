1c4169992f8811318324c30400c94b2000d72464
Eric Rhinebolt <55140312+Boltz1011@users.noreply.github.com>
2021-11-09 21:17:41 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 ///////////////////////////////////////////////////////////
-// Make mobile navigation work
-
-// const btnNavEl = document.querySelector(".btn-mobile-nav");
-// const headerEl = document.querySelector(".header");
-
-// btnNavEl.addEventListener("click", function () {
-//   headerEl.classList.toggle("nav-open");
-// });
-
-///////////////////////////////////////////////////////////
 // Slide in pages
 
 const logoLink = document.querySelector(".logo-link");
@@ -104,62 +94,6 @@ const expList2 = document.querySelectorAll(".list1").forEach((list1) => {
     event.target.firstElementChild?.classList.remove("show");
   });
 });
-
-///////////////////////////////////////////////////////////
-
-// Smooth scrolling animation
-
-// const allLinks = document.querySelectorAll("a:link");
-// console.log(allLinks);
-
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-
-//     // Close mobile nav
-//     if (link.classList.contains("main-nav-link"))
-//       headerEl.classList.toggle("nav-open");
-//   });
-// });
-
-///////////////////////////////////////////////////////////
-// Sticky navigation
-
-// const sectionHeroEl = document.querySelector(".section-hero");
-
-// const obs = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     console.log(ent);
-//     if (ent.isIntersecting === false) {
-//       document.body.classList.add("sticky");
-//     }
-//     if (ent.isIntersecting === true) {
-//       document.body.classList.remove("sticky");
-//     }
-//   },
-//   {
-//     // In the viewport
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-80px",
-//   }
-// );
-// obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
